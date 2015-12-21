a391d45c16851e166f5e1bb5527b55eeea030e67
tvsijin <sijinads003@gmail.com>
2015-12-21 23:34:18 +0530

    popover chats

--- client/scripts/chats.controller.js
+++ client/scripts/chats.controller.js
@@ -1,11 +1,21 @@
 angular.module('zeebra').controller('ChatsCtrl',ChatsCtrl);
 
-function ChatsCtrl ($scope, $reactive){
+function ChatsCtrl ($scope, $reactive, $ionicPopover){
 	$reactive(this).attach($scope);
-
+  this.showPopover = showPopover;
   this.helpers({
     data() {
       return Chats.find();
     }
   });
+
+  $ionicPopover.fromTemplateUrl('templates/popover.html', {
+    scope: $scope
+  }).then(function(pop) {
+    popover = pop;
+  });
+
+  function showPopover($event){
+  popover.show($event);
+  }
 }
